77b68292152756a3f5fca1adb1be4e5faaa0f900
Meghana-Katragadda <137162055+Meghana-Katragadda@users.noreply.github.com>
2023-10-05 15:54:40 +0530

    Add files via upload

---
+++ Scenario.spec.js
@@ -0,0 +1,24 @@
+const {test,expect}=require('@playwright/test')
+test('Scenario1',async({page})=>{
+await page.goto('https://www.lambdatest.com/selenium-playground')
+await page.click("//a[text()='Simple Form Demo']")
+const urllink=page.url()
+if(urllink.includes('simple-form-demo')){
+console.log('URL contains simple form demo')
+}
+else{
+    console.log('URL doesnot have simple form demo')
+}
+const message='Welcome to LambdaTest'
+await page.locator("//input[@id='user-message']").fill(message)
+await page.click("//button[text()='Get Checked Value']")
+const display=await page.locator("//p[@id='message']").innerText(message)
+if(display==message){
+    console.log(message)
+}
+else{
+    console.log('message is not matched')
+}
+await page.waitForTimeout(5000)
+})
+
